2a4a68dcf453b1590260fc7830f7c25a276803ed
Saurabh Shukla <saurabh.shukla244@gmail.com>
2019-10-01 19:47:46 +0530

    added fenwick tree

---
+++ fenwick-tree.cpp
@@ -0,0 +1,20 @@
+
+ll ft[N];
+
+void update(int x, int delta){
+
+      for(; x <= n; x += x&-x)
+        ft[x] += delta;
+}
+
+ll pref(int x){
+     ll sum = 0;
+     for(; x > 0; x -= x&-x)
+        sum += ft[x];
+
+     return sum;
+}
+
+ll query(int l, int r){
+	return pref(r) - pref(l-1);
+}
